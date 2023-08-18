f4e1373e74fdf4370be3aaa9a095f086243d54e9
MahedialiBhatt <bhattmahediali92@gmail.com>
2023-08-18 22:10:20 +0530

    Implemented auth endpoints

--- functions/src/components/api/auth/auth.controller.ts
+++ functions/src/components/api/auth/auth.controller.ts
@@ -8,7 +8,9 @@ import {
   dataNotExistException,
   unauthorizedException,
 } from '../../../utils/apiErrorHandler';
-// import { getUser, getUserByEmail, updateUserFields } from '../../../models/user';
+import { checkPassword, getUser, getUserByEmail, getUserById, updateUserFields } from '../../../models/user';
+import { addToken, deleteToken } from '../../../models/token';
+import { TokenDocument } from '../../../models/token/token.entity';
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -18,27 +20,39 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
     res.status(200).json();
   } catch (err: any) {
     logger.error(err);
-    next(err);
+    res.status(500).json({ message: err });
   }
 };
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { user_id } = req.user;
-    if (!user_id) throw badImplementationException('user_id is not set properly');
+    const { email, password } = req.body;
+    if (!email || !password) throw badImplementationException('email or password is not set properly');
 
     const { ACCESS_TOKEN_EXPIRED_IN, REFRESH_TOKEN_EXPIRED_IN } = process.env;
 
-    const accessToken = encodeJwt({ id: user_id }, ACCESS_TOKEN_EXPIRED_IN || '1m', 'access');
-    const refreshToken = encodeJwt({ id: user_id }, REFRESH_TOKEN_EXPIRED_IN || '2m', 'refresh');
+    const user = await getUserByEmail(email);
+    if (user.length === 0) throw dataNotExistException('Email does not register');
+    const passwordCheck = await checkPassword(email, password);
+    if (!passwordCheck) throw unauthorizedException('Password is not correct');
 
-    // TODO update refresh token
-    // await updateUserFields();
+    const accessToken = encodeJwt({ id: user[0].user_id }, ACCESS_TOKEN_EXPIRED_IN || '1m', 'access');
+    const refreshToken = encodeJwt({ id: user[0].user_id }, REFRESH_TOKEN_EXPIRED_IN || '2m', 'refresh');
 
+    await updateUserFields(user[0].user_id, { refresh_token: refreshToken, updated_at: getCurrentJST() });
+    const tokenToFirebase: TokenDocument = {
+      token_id: refreshToken,
+      user_id: user[0].user_id,
+      token_type: 'resetPassword',
+      user_type: 'user',
+      created_at: getCurrentJST(),
+      expired_at: '',
+    };
+    await addToken(tokenToFirebase);
     res.status(200).json({ accessToken, refreshToken });
   } catch (err: any) {
     logger.error(err);
-    next(err);
+    res.status(500).json({ message: err });
   }
 };
 
@@ -47,13 +61,12 @@ export const logout = async (req: Request, res: Response, next: NextFunction) =>
     const { user_id } = req.user;
     if (!user_id) throw badImplementationException('user_id is not set properly');
 
-    // TODO updateUser for make the refresh token to be null
-    // await updateUserFields();
-
-    res.status(200).json();
-  } catch (err) {
+    await updateUserFields(user_id, { refresh_token: null, updated_at: getCurrentJST() });
+    await deleteToken(req.headers['authorization'] ?? '');
+    res.status(200).json('Successfully logout');
+  } catch (err: any) {
     logger.error(err);
-    next(err);
+    res.status(500).json({ message: err });
   }
 };
 
@@ -61,18 +74,18 @@ export const forgotPassword = async (req: Request, res: Response, next: NextFunc
   try {
     const { email } = req.body;
 
-    // TODO implment getUserByEmail to get user detail
-    // const users = await getUserByEmail(email);
-    // if (users.length === 0) throw dataNotExistException('Email does not register');
-    // if (users.length > 1) throw badImplementationException('Something went wrong. Email is more than 1.');
-    // if (users[0].status !== 'active') throw unauthorizedException('This user is unauthorized.');
+    const users = await getUserByEmail(email);
 
-    // service.forgotPassword(users[0]);
+    if (users.length === 0) throw dataNotExistException('Email does not register');
+    if (users.length > 1) throw badImplementationException('Something went wrong. Email is more than 1.');
+    if (users[0].status !== 'active') throw unauthorizedException('This user is unauthorized.');
 
-    res.status(200).json();
+    service.forgotPassword(users[0]);
+
+    res.status(200).json('Successfully send email');
   } catch (err: any) {
     logger.error(err);
-    next(err);
+    res.status(500).json({ message: err });
   }
 };
 
@@ -85,7 +98,7 @@ export const updatePassword = async (req: Request, res: Response, next: NextFunc
     res.status(200).json();
   } catch (err: any) {
     logger.error(err);
-    next(err);
+    res.status(500).json({ message: err });
   }
 };
 
@@ -95,23 +108,26 @@ export const refresh = async (req: Request, res: Response, next: NextFunction) =
 
     const decoded = decodeJwt(refreshToken, 'refresh');
 
-    // TODO get user by id
-    // const user = await getUser(decoded.payload.id);
-    // if (!user) throw unauthorizedException('User is not exist');
-    // if (user.status !== 'active') throw unauthorizedException('This user is not active');
-    // if (user.refreshToken !== refreshToken) throw unauthorizedException('Refresh token is not valid');
+    if (typeof decoded === 'string') {
+      throw new Error('Invalid JWT token');
+    }
+
+    const user = await getUserById(decoded.id);
+    if (!user) throw unauthorizedException('User is not exist');
+    if (user.status !== 'active') throw unauthorizedException('This user is not active');
+    if (user.refresh_token !== refreshToken) throw unauthorizedException('Refresh token is not valid');
 
     const { ACCESS_TOKEN_EXPIRED_IN, REFRESH_TOKEN_EXPIRED_IN } = process.env;
 
-    // const accessToken = encodeJwt({ id: user.user_id }, ACCESS_TOKEN_EXPIRED_IN || '5m', 'access');
-    // const newRefreshToken = encodeJwt({ id: user.user_id }, REFRESH_TOKEN_EXPIRED_IN || '30d', 'refresh');
+    const accessToken = encodeJwt({ id: user.user_id }, ACCESS_TOKEN_EXPIRED_IN || '5m', 'access');
+    const newRefreshToken = encodeJwt({ id: user.user_id }, REFRESH_TOKEN_EXPIRED_IN || '30d', 'refresh');
 
     // update refresh token
-    // await updateUserFields();
+    await updateUserFields(user.user_id, { refresh_token: newRefreshToken, updated_at: getCurrentJST() });
 
-    // res.status(200).json({ accessToken, refreshToken: newRefreshToken });
+    res.status(200).json({ accessToken, refreshToken: newRefreshToken });
   } catch (err: any) {
     logger.error(err);
-    next(err);
+    res.status(500).json({ message: err });
   }
 };
--- functions/src/components/api/auth/auth.service.ts
+++ functions/src/components/api/auth/auth.service.ts
@@ -13,11 +13,39 @@ import { getAddToCurrentJST, getCurrentJST } from '../../../utils/dayjs';
 import { TokenDocument } from '../../../models/token/token.entity';
 import { addToken, deleteToken, getToken } from '../../../models/token';
 import { MESSAGE_RESET_PASSWORD } from './auth.message';
-// import { addUser, getUserByEmail, updateUserFields } from '../../../models/user';
-// import { UserDocument } from '../../../models/user/user.entity';
+import { addUser, getUserByEmail, updateUserFields } from '../../../models/user';
+import { UserDocument } from '../../../models/user/user.entity';
 
 export const createUser = async (email: string, password: string, name: string, phone: string, address: string) => {
-  // TODO
+  let error: Error | HttpException | undefined;
+  try {
+    const user = await getUserByEmail(email);
+    if (user.length) throw dataConflictException('Email already exists');
+
+    const getHashPassword = await hashPassword(password);
+
+    const newUser: UserDocument = {
+      user_id: uuidv4(),
+      email,
+      password: getHashPassword,
+      name,
+      phone,
+      address,
+      status: 'active',
+      refresh_token: null,
+      created_at: getCurrentJST(),
+      updated_at: getCurrentJST(),
+      deleted_at: null,
+    };
+
+    await addUser(newUser);
+
+    return Promise.resolve('User Register successfully');
+  } catch (err) {
+    logger.error(err);
+    error = err instanceof Error ? err : badImplementationException(err);
+    return Promise.reject(error);
+  }
 };
 
 export const forgotPassword = async (user: UserDocument) => {
@@ -38,7 +66,7 @@ export const forgotPassword = async (user: UserDocument) => {
 
     await sendMessage(MESSAGE_RESET_PASSWORD(user.email, tokenUrl));
 
-    return Promise.resolve('success');
+    return Promise.resolve('Success');
   } catch (err) {
     logger.error(err);
     error = err instanceof Error ? err : badImplementationException(err);
@@ -54,12 +82,11 @@ export const updatePassword = async (password: string, tokenId: string) => {
     if (token.user_type !== 'user') throw invalidException('Token is not valid user type');
     if (token.token_type !== 'resetPassword') throw invalidException('Token is not valid token type');
 
-    // TODO
-    // await updateUserFields(token.user_id, { password: hashPassword(password), updated_at: getCurrentJST() });
+    await updateUserFields(token.user_id, { password: await hashPassword(password), updated_at: getCurrentJST() });
 
     await deleteToken(tokenId);
 
-    return Promise.resolve();
+    return Promise.resolve('Password Updated successfully');
   } catch (err) {
     console.log(err);
     error = err instanceof Error ? err : badImplementationException(err);
