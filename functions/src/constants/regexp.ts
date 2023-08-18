6aa735c5aa21522c6ffccbcc6f4cf76830fe735d
MahedialiBhatt <bhattmahediali92@gmail.com>
2023-08-18 22:07:11 +0530

    Implemented TODOs

--- functions/src/constants/regexp.ts
+++ functions/src/constants/regexp.ts
@@ -1,8 +1,14 @@
 /**
  * @description At least one special char, one lowercase, one uppercase, one number
  */
-export const REGEXP_PASSWORD = /^/; //TODO
+export const REGEXP_PASSWORD = /^(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/;
+
 /**
  * @description YYYY-MM-DD HH:MM:SS+HH:MM
  */
-export const REGEXP_DATETIME = /^/; // TODO
+export const REGEXP_DATETIME = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\+\d{2}:\d{2}$/;
+
+/**
+ * @description example@gmail.com
+ */
+export const REGEXP_EMAIL = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
--- functions/src/constants/validation.ts
+++ functions/src/constants/validation.ts
@@ -1,7 +1,76 @@
 import { ParamSchema, Location } from 'express-validator';
+import { REGEXP_EMAIL, REGEXP_PASSWORD } from './regexp';
 
 export const VALIDATION_STRING = (where: Location): ParamSchema => ({
   in: [where],
   isString: true,
   notEmpty: true,
+});
+
+export const VALIDATION_EMAIL_NOT_EXIST = (where: Location): ParamSchema => ({
+  in: [where],
+  isString: true,
+  notEmpty: true,
+  isEmail: true,
+});
+
+export const VALIDATION_PASSWORD = (where: Location): ParamSchema => ({
+  in: [where],
+  isString: true,
+  notEmpty: true,
+  isLength: {
+    errorMessage: 'Password should be at least 8 chars long',
+    options: { min: 8 },
+  },
+  matches: {
+    options: REGEXP_PASSWORD,
+    errorMessage: 'Password should be at least one special char, one lowercase, one uppercase, one number',
+  },
+});
+
+export const VALIDATION_ACCOUNT_TEL = (where: Location): ParamSchema => ({
+  in: [where],
+  isString: true,
+  notEmpty: true,
+  isLength: {
+    errorMessage: 'Phone number should be at least 10 chars long',
+    // Multiple options would be expressed as an array
+    options: { min: 10 },
+  },
+  matches: {
+    options: /^[0-9]+$/,
+    errorMessage: 'Phone number should be number',
+  },
+});
+
+export const VALIDATION_EMAIL_EXIST = (where: Location): ParamSchema => ({
+  in: [where],
+  isString: true,
+  notEmpty: true,
+  isEmail: true,
+  matches: {
+    options: REGEXP_EMAIL,
+    errorMessage: 'Email is not valid',
+  },
+});
+
+export const VALIDATION_PASSWORD_CHECK = (where: Location, email: string): ParamSchema => ({
+  in: [where],
+  isString: true,
+  notEmpty: true,
+  matches: {
+    options: REGEXP_PASSWORD,
+    errorMessage: 'Password should be at least one special char, one lowercase, one uppercase, one number',
+  },
+});
+
+export const VALIDATION_TOKEN = (where: Location): ParamSchema => ({
+  in: [where],
+  isString: true,
+  notEmpty: true,
+  isLength: {
+    errorMessage: 'Token should be at least 10 chars long',
+    // Multiple options would be expressed as an array
+    options: { min: 10 },
+  },
 });
